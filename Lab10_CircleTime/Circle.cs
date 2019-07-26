22205599f13736a328fb8e8e40b01450937756b1
zeL <3DenzeL@protonmail.com>
2019-07-26 01:08:58 -0400

    first commit for lab #10

---
+++ Lab10_CircleTime/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab10_CircleTime
+{
+    public class Circle
+    {
+        #region Fields
+        private double radius;
+        #endregion
+
+        #region Properties
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+      
+
+        public Circle(double _radius)
+        {
+            radius = _radius;
+        }
+        #endregion
+
+
+        #region Methods
+        public double CalculateArea(double input)
+        {
+            return Math.Round(Math.PI*(Math.Pow(radius,2)),2);
+        }
+
+        public double CalculateCircumference(double input)
+        {
+            return Math.Round((2 * Math.PI * radius),2);
+        }
+        #endregion
+    }
+}
---
+++ Lab10_CircleTime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab10_CircleTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            bool go = true;
+            while (go)
+            {
+                Console.Write("Please enter a radius! \t");
+                try
+                {
+                    double input = double.Parse(Console.ReadLine());
+                    Circle circle = new Circle(input);
+
+                    double circumference = circle.CalculateCircumference(input);
+                    double area = circle.CalculateArea(input);
+
+                    Console.WriteLine($"\nCircle Area: {area}");
+                    Console.WriteLine($"Cirle Circumference: {circumference}");
+                    count++;
+                }
+                catch
+                {
+                    Console.WriteLine("\nWhoa there! Gotta enter a number!");
+                }
+
+                Console.Write("\ncontinue? [y/n] \t");
+                string userChoice = Console.ReadLine();
+
+                if (userChoice == "y" || userChoice == "yes")
+                {
+
+                    go = true;
+                }
+                else if (userChoice == "n" || userChoice == "no")
+                {
+                    Console.WriteLine($"\nYou made {count} circles. Later!");
+                    go = false;
+                }
+            }
+        }
+    }
+}
